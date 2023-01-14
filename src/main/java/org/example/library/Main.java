574eb7b25318cc86a27a43d34fb3fb2531056e7d
Filip Sokolowski <filipsokolowskiah@gmail.com>
2023-01-14 10:09:39 +0100

    first config

---
+++ src/main/java/org/example/library/Main.java
@@ -0,0 +1,8 @@
+package org.example.library;
+
+public class Main {
+    public static void main(String[] args) {
+        String TestCheckstyle = "test";
+        System.out.println(TestCheckstyle);
+    }
+}
