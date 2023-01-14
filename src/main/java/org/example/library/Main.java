5c6e8f5270bc652cec061905eb5c5cdd1dc11f27
Filip Sokolowski <filipsokolowskiah@gmail.com>
2023-01-14 11:10:59 +0100

    checkstyle fix

--- src/main/java/org/example/library/Main.java
+++ src/main/java/org/example/library/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         User user = new User(1, "login", "pass", "name");
 
-        String TestCheckstyle = "test";
-        System.out.println(TestCheckstyle);
+        String testCheckstyle = "test";
+        System.out.println(testCheckstyle);
     }
 }
